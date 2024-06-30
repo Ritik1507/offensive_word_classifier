9c31456d2275bfc87fbe810b68547844c1d9569f
ritik1507 <112415038+Ritik1507@users.noreply.github.com>
2024-06-30 06:38:59 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "offensive_word_classifier",
+    version="0.0.1",
+    author="Ritik",
+    author_email="ritikpadhi1507@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
